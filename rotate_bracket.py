927a1e026cdf2dc810219fa37dd1a15e21e7940d
CheolWoong Hyun <44190898+hcw2155@users.noreply.github.com>
2021-08-06 02:26:00 +0900

    Update rotate_bracket.py
    100/100 solved

--- rotate_bracket.py
+++ rotate_bracket.py
@@ -1,32 +1,21 @@
+from collections import deque
 def solution(s):
-    
-    left = ['(','{','[']
-    right = [')','}',']']
+
+    dir = ['()','[]','{}']
     answer = 0
     
     for i in range(0,len(s)):
         temp = s[i:] + s[0:i]
-        cnt = [0,0,0]
-        flag = 0
-        for j in temp:
-            if j in left:
-                if j == '(':
-                    cnt[0] += 1
-                elif j == '{':
-                    cnt[1] += 1
-                elif j == '[':
-                    cnt[2] += 1
-            else:
-                if j == ')':
-                    cnt[0] -= 1
-                elif j == '}':
-                    cnt[1] -= 1
-                elif j == ']':
-                    cnt[2] -= 1
-            for i in cnt:
-                if i < 0 :
-                    flag = 1
-        if flag == 0 and cnt.count(0) == 3:
-            answer += 1
-                
+        temp = deque(temp)
+        stack = []
+        stack = deque(stack)
+        while len(temp) != 0:
+            stack.append(temp.popleft())
+            if len(stack) >= 2:
+                if stack[-2] + stack[-1] in dir:
+                    stack.pop()
+                    stack.pop()
+        if len(stack) == 0:
+            answer +=1
+            
     return answer
