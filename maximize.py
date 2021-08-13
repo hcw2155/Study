69e263fe783a01ceda92c2cb57e9d9ec88f1104f
CheolWoong Hyun <44190898+hcw2155@users.noreply.github.com>
2021-08-13 15:58:01 +0900

    Update maximize.py

--- maximize.py
+++ maximize.py
@@ -1,41 +1,46 @@
+# max
 from itertools import permutations
 
-def cal(op, a, b):
+def operation(num1, num2, op):
     if op == '+':
-        return a+b
-    elif op == '-':
-        return a-b
-    elif op == '*':
-        return a*b
-
-def change(s):
-    s += '.'
-    cal = ['+','-','*']
-    result = []
-    temp = ''
-    for i in s:
-        if i in cal:
-            result.append(temp)
-            result.append(i)
-            temp = ''
-        elif i == '.':
-            result.append(temp)
-            break
+        return str(int(num1) + int(num2))
+    if op == '-':
+        return str(int(num1) - int(num2))
+    if op == '*':
+        return str(int(num1) * int(num2))
+    
+def calculate(exp,op):
+    array=[]
+    tmp=""
+    for i in exp:
+        if i.isdigit()==True:
+            tmp+=i
         else:
-            temp += i
-    return result
+            array.append(tmp)
+            array.append(i)
+            tmp=""
+    array.append(tmp)
+    print(array,'array')
+    for o in op:
+        stack=[]
+        while len(array)!=0:
+            tmp=array.pop(0)
+            if tmp==o:
+                stack.append(operation(stack.pop(), array.pop(0), o))
+            else:
+                stack.append(tmp)
+        array=stack
+            
+    return abs(int(array[0]))
 
 def solution(expression):
-    answer = 0
-    temp = ''
-    op = list(permutations(['+','-','*']))
-    new = change(expression)
-    print(new)
-    for i in operation:
-        pass
-  
-    return answer
+    op = ['+', '-', '*']
+    op = list(permutations(op, 3))
+    result=[]
+    for i in op:
+        result.append(calculate(expression, i))
+    return max(result)
 
-
+    
 expression = "100-200*300-500+20"
-solution(expression)
+print(calculate(expression,['+','-','*']))
