afdc2859c649629f6a1cbcd3ed7d66a48f26a301
CheolWoong Hyun <44190898+hcw2155@users.noreply.github.com>
2021-08-16 19:00:32 +0900

    Update openchat.py

--- openchat.py
+++ openchat.py
@@ -1,28 +1,44 @@
 def solution(record):
     answer = []
     result = []
-    id = []
-
+    id = {}
+    # Enter uid1234 Muzi / Etner Leave Change
     for i in record:
         i = i.split(" ")
-        if i[1] not in id:
-            id.append(i[1])
-        result.append(i)
-    temp_name = ''
-    final_name = []
-    
-    for i in id:  
-        for j in result:
-            if len(j) > 2 and i == j[1]: 
-                temp_name = j[2]
-        final_name.append([i,temp_name])
-        temp_name = ''
-        
+        if len(i) > 2:
+            id[i[1]] = i[2]
+        result.append([i[0],i[1]])
+    print(id, result)
+
     for i in result:
-        for j in final_name:
-            if i[0] == 'Enter' and j[0] == i[1]:
-                answer.append('%s님이 들어왔습니다.'%(j[1]))
-            elif i[0] == 'Leave' and j[0] == i[1]:
-                answer.append('%s님이 나갔습니다.'%(j[1]))
+        if i[0] == 'Enter':
+            answer.append('%s님이 들어왔습니다.'%id.get(i[1]))
+        elif i[0] == 'Leave':
+            answer.append('%s님이 나갔습니다.'%id.get(i[1]))
+
+
+    # for i in record:
+    #     i = i.split(" ")
+    #     if i[1] not in id:
+    #         id.append(i[1])
+    #     result.append(i)
+    # temp_name = ''
+    # final_name = []
+    
+    # for i in id:  
+    #     for j in result:
+    #         if len(j) > 2 and i == j[1]: 
+    #             temp_name = j[2]
+    #     final_name.append([i,temp_name])
+    #     temp_name = ''
+        
+    # for i in result:
+    #     for j in final_name:
+    #         if i[0] == 'Enter' and j[0] == i[1]:
+    #             answer.append('%s님이 들어왔습니다.'%(j[1]))
+    #         elif i[0] == 'Leave' and j[0] == i[1]:
+    #             answer.append('%s님이 나갔습니다.'%(j[1]))
 
     return answer
+record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+print(solution(record))
