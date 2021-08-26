7d6120fffeea7a0d94594a9ae6e73e6da5f0d727
CheolWoong Hyun <44190898+hcw2155@users.noreply.github.com>
2021-08-26 20:06:38 +0900

    Update friends4block.py
    solved

--- friends4block.py
+++ friends4block.py
@@ -1,28 +1,23 @@
 def solution(m, n, board):
     answer = 0
     p = [[0 for _ in range(len(board[0]))] for _ in range(len(board))]
-    # p = [[0] * n for _ in range(m)]
-    block = ['R','M','A','F','N','T','J','C','B']
     new = []
     for i in range(0,m):
         tmp = []
         for j in range(0,n):
             tmp.append(board[i][j])
         new.append(tmp)
-    # for i in range(len(board)):
-    #     board[i] = list(board[i])
 
     while True:
         check = []
         for i in range(0,m-1):
             for j in range(0,n-1):
-                if new[i][j] in block:
-                    if new[i][j] == new[i+1][j] == new[i][j+1] == new[i+1][j+1]:
-                        check.append((i,j))
-                        check.append((i,j+1))
-                        check.append((i+1,j))
-                        check.append((i+1,j+1))
-        print(new,check)
+                # if new[i][j] in block:
+                if new[i][j] != 'X' and new[i][j] == new[i+1][j] == new[i][j+1] == new[i+1][j+1]:
+                    check.append((i,j))
+                    check.append((i,j+1))
+                    check.append((i+1,j))
+                    check.append((i+1,j+1))
         if len(check) == 0:
             break
         else:
@@ -37,7 +32,6 @@ def solution(m, n, board):
                         new[put_n][c[1]] = new[check_n][c[1]]
                         new[check_n][c[1]] = 'X'
                         put_n -= 1
-                        print(put_n,'put')
                     check_n -= 1
     return answer
 
